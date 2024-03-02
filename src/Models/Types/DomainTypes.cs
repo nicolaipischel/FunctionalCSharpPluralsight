aba1dd149ff632eca5a6d76b1beba76f06993a07
Nicolai Pischel <nicolai.pischel@live.de>
2024-03-02 16:50:30 +0100

    Implemented the needed Types for the customers business domain

---
+++ src/Models/Types/DomainTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace Models.Types;
+
+public record StockKeepingUnit(string Value);
+public record Vendor(Guid Id, string Name);
+public record ExternalSkuPhoto(byte[] Content, string MimeType, Vendor Vendor);
+public record Part(string Name, StockKeepingUnit Sku);
+public record ExternalSku(StockKeepingUnit Sku, Vendor Vendor);
+public record ExternalPart(Part Part, ExternalSku Sku);
