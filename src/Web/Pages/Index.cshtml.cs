5c66b88c9bef1d45ecc0dd36e3d2dd9de93fc951
Nicolai Pischel <nicolai.pischel@live.de>
2024-03-02 17:39:12 +0100

    Created Component for displaying parts in the UI

--- src/Web/Pages/Index.cshtml.cs
+++ src/Web/Pages/Index.cshtml.cs
@@ -1,19 +1,24 @@
-using Microsoft.AspNetCore.Mvc;
+using Application;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models.Types;
 
 namespace Web.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IReadOnlyRepository<Part> _parts;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IReadOnlyRepository<Part> parts)
     {
         _logger = logger;
+        _parts = parts;
     }
+
+    public IEnumerable<Part> AllParts { get; set; } = Enumerable.Empty<Part>();
 
     public void OnGet()
     {
-
+        this.AllParts = _parts.GetAll().ToList();
     }
 }
