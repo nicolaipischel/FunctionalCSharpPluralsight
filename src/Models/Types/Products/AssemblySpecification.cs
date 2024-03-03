e58879a6682b7dc4fe4f3890d7fafd26b8bf7592
Nicolai Pischel <nicolai.pischel@live.de>
2024-03-03 15:06:54 +0100

    Implemented Instructions on an AssemblySpecification using ImmutableList

--- src/Models/Types/Products/AssemblySpecification.cs
+++ src/Models/Types/Products/AssemblySpecification.cs
@@ -1,17 +1,27 @@
-﻿using Models.Types.Common;
+﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using Models.Types.Common;
 using Models.Types.Components;
 
 namespace Models.Types.Products;
 
 public class AssemblySpecification
 {
-    public AssemblySpecification(Guid id) => Id = id;
-    
+    public AssemblySpecification(Guid id) =>
+        (Id, Instructions) = (id, ImmutableList<AssemblyInstruction>.Empty);
+
+    [SetsRequiredMembers]
     public AssemblySpecification(AssemblySpecification other) =>
-        (Id, Name, Description) = (other.Id, other.Name, other.Description);
+        (Id, Name, Description, Instructions) =
+        (other.Id, other.Name, other.Description, other.Instructions);
     public Guid Id { get; init; }
     public required string Name { get; init; }
     public required string Description { get; init; }
+    
+    private ImmutableList<AssemblyInstruction> Instructions { get; init; }
+
+    public AssemblySpecification Add(params AssemblyInstruction[] instructions) =>
+        new(this) { Instructions = this.Instructions.AddRange((instructions)) };
 
     public IEnumerable<(Part part, DiscreteMeasure quantity)> Components { get; init; } =
         Enumerable.Empty<(Part, DiscreteMeasure)>();
